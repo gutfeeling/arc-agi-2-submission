b3d252cefeef1ca64664807897117b2f7212c2a6
Dibya Chakravorty <dibyachakravorty@gmail.com>
2026-01-13 02:46:14 +0100

    fix: didn't set US sandboxes in one place

--- src/arcagi2/solver/config/gpt_5_2_xhigh.py
+++ src/arcagi2/solver/config/gpt_5_2_xhigh.py
@@ -64,7 +64,7 @@ GPT_5_2_XHIGH_SYSTEM_CONFIG = InterleavedThinkingConfig(
 
 GPT_5_2_XHIGH_DAYTONA_SYSTEM_CONFIG = InterleavedThinkingConfig(
     sandbox_cls=DAYTONA_SANDBOX_CLS,
-    sandbox_kwargs=DAYTONA_SANDBOX_KWARGS,
+    sandbox_kwargs={**DAYTONA_SANDBOX_KWARGS, "target": "us"},    # Use US sandbox for Kaggle evaluation
     interleaved_thinking_solver=AsyncResponsesAPIClient.ResponsesAPICallConfig(
         **_DAYTONA_COMMON_KWARGS,
         prompt_path=PROMPTS_FOLDER / "interleaved_thinking_solver.txt"
